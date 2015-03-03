876e03e904232faf8675b4d7f90d772820ba2309
9seconds <nineseconds@yandex.ru>
2015-03-03 11:47:55 +0300

    Add colorized mans

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -218,6 +218,19 @@ filespu() {
     filesp "$1" | awk 'NR > 1 {print $2}' | sort -u
 }
 
+man() {
+    # This is a small wrapper for colorizing man pages.
+
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+        LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+        LESS_TERMCAP_me=$'\E[0m' \
+        LESS_TERMCAP_se=$'\E[0m' \
+        LESS_TERMCAP_so=$'\E[38;5;246m' \
+        LESS_TERMCAP_ue=$'\E[0m' \
+        LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man $@
+}
+
 vim_cmd() {
     # Executes VIM command without a VIM start.
     #
