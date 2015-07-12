889983dac54dbeac3a69d993f1f0161a3877f24b
9seconds <nineseconds@yandex.ru>
2015-07-12 11:35:33 +0300

    Modify tmux conf

--- zsh/env.sh
+++ zsh/env.sh
@@ -38,6 +38,7 @@ export PATH="$PATH:$GOBIN"
 export PATH="$HOME/.gem/ruby/current/bin:$PATH"
 
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
+#export NVIM_TUI_ENABLE_CURSOR_SHAPE=1
 
 # Set proper TERM for tmux
 if [ -n "$TMUX" ]; then
