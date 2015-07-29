b51280b67b4f976f246eadd83147bc44f68c930c
9seconds <nineseconds@yandex.ru>
2015-07-29 22:00:34 +0300

    Purge old kernel function

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -487,3 +487,10 @@ allup() {
         wait
     fi
 }
+
+purgeoldkernels() {
+    # Purges old Ubuntu kernels
+    # http://askubuntu.com/a/254585
+
+    echo $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs sudo apt-get -y purge
+}
