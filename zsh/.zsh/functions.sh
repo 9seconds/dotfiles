9dcb0803cb30f95f9424d1ce353a1b41d0bb7d42
9seconds <nineseconds@yandex.ru>
2020-08-17 10:21:35 +0300

    Remove most of the zsh functions

--- zsh/.zsh/functions.sh
+++ zsh/.zsh/functions.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env zsh
 
-aptup() {
-    sudo apt update \
-    && sudo apt full-upgrade --yes \
-    && sudo apt autoremove --yes --purge \
-    && sudo apt autoclean \
-    && sudo snap refresh
-}
-
-brewup() {
-    brew update && brew upgrade && brew cask upgrade --greedy && brew cleanup
-}
-
-purgeoldkernels() {
-    # Purges old Ubuntu kernels
-    # http://askubuntu.com/a/254585
-
-    echo $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs sudo apt-get -y purge
-}
-
 reswap() {
     sudo swapoff -a && sudo swapon -a
 }
