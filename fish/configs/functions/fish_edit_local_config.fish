da22ed36390a2a3daebae8e3aa274a4b884a7384
9seconds <nineseconds@yandex.ru>
2021-04-20 13:56:03 +0000

    Remove fish_edit_local_config function

--- fish/configs/functions/fish_edit_local_config.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-#
-# This file opens editor for a local config.
-
-function fish_edit_local_config -d 'Edit local config'
-  command $EDITOR $HOME/.local-config.fish; and source $HOME/.local-config.fish
-end
