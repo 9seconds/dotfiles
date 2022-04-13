0bc41814fcaddc2df4c0335abb6725483d3022aa
9seconds <nineseconds@yandex.ru>
2022-04-13 10:13:36 +0000

    Add #T and #F snippets

--- neovim/lua/_snippet_utils.lua
+++ neovim/lua/_snippet_utils.lua
@@ -32,4 +32,57 @@ function M.get_ts_scope_at_cursor()
 end
 
 
+M.get_git_config = (function()
+  local job = require("plenary.job")
+
+  local cache = {}
+
+  return function()
+    local gitdir = ""
+    local cwd = vim.fn.getcwd()
+    local cachekey = cwd
+
+    if vim.b.gitsigns_status_dict then
+      gitdir = vim.b.gitsigns_status_dict.gitdir
+      cachekey = gitdir
+    end
+
+    if cache[cachekey] then
+      return cache[cachekey]
+    end
+
+    local args = {"-P"}
+    if gitdir then
+      table.insert(args, "--git-dir")
+      table.insert(args, gitdir)
+    end
+    table.insert(args, "config")
+    table.insert(args, "--list")
+
+    local cmd = job:new({
+      command="git",
+      args=args,
+      cwd=cwd,
+      enable_recording=true,
+    })
+
+    local stdout, code = cmd:sync()
+    if code ~= 0 then
+      return {}
+    end
+
+    local config = {}
+
+    for _, line in ipairs(stdout) do
+      local chunks = vim.split(line, "=", {plain=true, trimempty=true})
+      config[chunks[1]] = table.concat(vim.list_slice(chunks, 2), "=")
+    end
+
+    cache[cachekey] = config
+
+    return config
+  end
+end)()
+
+
 return M
--- neovim/snippets/all.lua
+++ neovim/snippets/all.lua
@@ -2,6 +2,38 @@ local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
 local d = ls.dynamic_node
+local f = ls.function_node
+
+
+local function make_special_comment(title)
+  return function()
+      local snippet_utils = require("_snippet_utils")
+
+      local fmt_string = vim.api.nvim_get_option_value("cms", {}) or "# %s"
+      local git_config = snippet_utils.get_git_config()
+
+      local nodes = {t(fmt_string:format(title))}
+      if git_config["user.name"] then
+        table.insert(nodes, f(function(args)
+          if args[1][1] ~= "" then
+            return "("
+          end
+          return ""
+        end, {1}))
+        table.insert(nodes, i(1, git_config["user.name"]))
+        table.insert(nodes, f(function(args)
+          if args[1][1] ~= "" then
+            return ")"
+          end
+          return ""
+        end, {1}))
+      end
+
+      table.insert(nodes, t(": "))
+
+      return sn(nil, nodes)
+  end
+end
 
 
 return {
@@ -26,5 +58,13 @@ return {
         i(1, filetype)
       })
     end, {1})
+  }),
+
+  s({trig="#T", dscr="TODO comment"}, {
+    d(1, make_special_comment("TODO"))
+  }),
+
+  s({trig="#F", dscr="FIXME comment"}, {
+    d(1, make_special_comment("FIXME"))
   })
 }
