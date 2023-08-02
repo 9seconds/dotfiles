85f695ecff0595b39cb6b1d4c95d730ac77ec3af
9seconds <nineseconds@yandex.ru>
2023-08-02 11:32:42 +0000

    Lazy load treesitter

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,81 +10,83 @@ return {
     "nvim-treesitter/nvim-treesitter-refactor",
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
+  event = "VeryLazy",
+  main = "nvim-treesitter.configs",
 
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
-      sync_install = true,
-
-      highlight = {
-        enable = true
-      },
-
-      incremental_selection = {
-        enable = true
-      },
-
-      indent = {
-        enable = true
-      },
-
-      -- nvim-treesitter-textobjects
-      -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-      textobjects = {
-        swap = {
-          enable = true,
-
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
-        },
-
-        select = {
-          enable = true,
-
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["ia"] = "@parameter.inner",
-            ["aa"] = "@parameter.outer",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-          }
-        }
-      },
-
-      -- nvim-treesitter-refactor
-      -- https://github.com/nvim-treesitter/nvim-treesitter-refactor
-      refactor = {
-        highlight_definitions = {
-          enable = true,
-
-          clear_on_cursor_move = true,
-        },
-
-        smart_rename = {
-          enable = true,
-
-          keymaps = {
-            smart_rename = "grr",
-          },
-        }
-      },
-
-      -- set commentstring based on a position in a file
-      -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-      context_commentstring = {
-        enable = true,
-      },
-    })
-
-    vim.o.foldmethod= "expr"
+  init = function()
+    vim.o.foldmethod = "expr"
     vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-  end
+  end,
+
+  opts = {
+    ensure_installed = "all",
+    sync_install = true,
+
+    highlight = {
+      enable = true
+    },
+
+    incremental_selection = {
+      enable = true
+    },
+
+    indent = {
+      enable = true
+    },
+
+    -- nvim-treesitter-textobjects
+    -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    textobjects = {
+      swap = {
+        enable = true,
+
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+
+      select = {
+        enable = true,
+
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["ia"] = "@parameter.inner",
+          ["aa"] = "@parameter.outer",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+        }
+      }
+    },
+
+    -- nvim-treesitter-refactor
+    -- https://github.com/nvim-treesitter/nvim-treesitter-refactor
+    refactor = {
+      highlight_definitions = {
+        enable = true,
+
+        clear_on_cursor_move = true,
+      },
+
+      smart_rename = {
+        enable = true,
+
+        keymaps = {
+          smart_rename = "grr",
+        },
+      }
+    },
+
+    -- set commentstring based on a position in a file
+    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+    context_commentstring = {
+      enable = true,
+    },
+  }
 }
