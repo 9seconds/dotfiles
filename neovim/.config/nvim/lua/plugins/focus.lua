8b3a53a9b252eb35b8220bd3580a2b834722ac5b
9seconds <nineseconds@yandex.ru>
2023-08-09 14:29:59 +0000

    Configure focus

--- neovim/.config/nvim/lua/plugins/focus.lua
+++ neovim/.config/nvim/lua/plugins/focus.lua
@@ -2,10 +2,44 @@
 -- https://github.com/nvim-focus/focus.nvim
 
 
+local ignore_filetypes = {
+  "neo-tree",
+}
+local ignore_buftypes = {
+  "nofile",
+  "prompt",
+  "popup",
+}
+
+
 return {
   "nvim-focus/focus.nvim",
   version = "*",
   event = "VeryLazy",
 
-  opts = {}
+  config = function()
+    require("focus").setup({})
+
+    local augroup = vim.api.nvim_create_augroup("9_Focus", {})
+
+    vim.api.nvim_create_autocmd("WinEnter", {
+      group = augroup,
+      callback = function()
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+          vim.b.focus_disable = true
+        end
+      end,
+      desc = "Disable focus autoresize for BufType",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = augroup,
+      callback = function()
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+          vim.b.focus_disable = true
+        end
+      end,
+      desc = "Disable focus autoresize for FileType",
+    })
+  end
 }
