8a2481948256466cc670c0f9fd2e5b04e6dd42dc
9seconds <nineseconds@yandex.ru>
2023-09-18 08:19:56 +0000

    Use natural sort in nvim-tree

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -2,6 +2,33 @@
 -- https://github.com/nvim-tree/nvim-tree.lua/
 
 
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#sorting-files-naturally-respecting-numbers-within-files-names
+local function natural_cmp(left, right)
+	left = left.name:lower()
+	right = right.name:lower()
+
+	if left == right then
+		return false
+	end
+
+	for i = 1, math.max(string.len(left), string.len(right)), 1 do
+		local l = string.sub(left, i, -1)
+		local r = string.sub(right, i, -1)
+
+		if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
+			local l_number = tonumber(string.match(l, "^[0-9]+"))
+			local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+			if l_number ~= r_number then
+				return l_number < r_number
+			end
+		elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+			return l < r
+		end
+	end
+end
+
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -28,9 +55,11 @@ return {
   end,
 
   opts = {
+    sort_by = function(nodes)
+      table.sort(nodes, natural_cmp)
+    end,
     disable_netrw = true,
     sync_root_with_cwd = true,
-    respect_buf_cwd = true,
     renderer = {
       highlight_git = true,
       highlight_modified = "name",
@@ -46,6 +75,9 @@ return {
     },
     filters = {
       dotfiles = true,
+      custom = {
+        "^.git$",
+      }
     },
   }
 }
