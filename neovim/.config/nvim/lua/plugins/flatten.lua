f6ca2bb970448802f09313e1dfb396051665cfae
9seconds <nineseconds@yandex.ru>
2024-01-11 16:19:07 +0000

    Add flatten nvim

---
+++ neovim/.config/nvim/lua/plugins/flatten.lua
@@ -0,0 +1,10 @@
+-- support nested terminals
+-- https://github.com/willothy/flatten.nvim
+
+return {
+  "willothy/flatten.nvim",
+  version = "*",
+  config = true,
+  lazy = false,
+  priority = 1001,
+}
