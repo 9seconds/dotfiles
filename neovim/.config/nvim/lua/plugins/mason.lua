d292404fa4dddb5e95dbfe1c95211e086cd0819e
9seconds <nineseconds@yandex.ru>
2024-01-29 10:14:04 +0000

    Remove tools installer

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -1,35 +1,10 @@
 -- LSP configuration for mason
 -- https://github.com/williamboman/mason.nvim
 
-local mason_config = {
+return {
   "williamboman/mason.nvim",
   version = "*",
   build = ":MasonUpdate",
 
   opts = {},
-}
-
-local mason_tool_installer = {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-  },
-
-  opts = {
-    ensure_installed = {
-      "black",
-      "efm",
-      "gitlint",
-      "hadolint",
-      "markdownlint",
-      "prettier",
-      "shellcheck",
-      "yamllint",
-    },
-  },
-}
-
-return {
-  mason_config,
-  mason_tool_installer,
 }
