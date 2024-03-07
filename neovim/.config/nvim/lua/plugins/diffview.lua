b8b491242909352d9f18369e9d6431f433713de5
9seconds <nineseconds@yandex.ru>
2024-03-07 08:42:46 +0000

    Add diffview

---
+++ neovim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,24 @@
+-- diffview
+-- https://github.com/sindrets/diffview.nvim
+
+return {
+  "sindrets/diffview.nvim",
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewFileHistory",
+    "DiffviewClose",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+    "DiffviewRefresh",
+    "DiffviewLog"
+  },
+
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      merge_tool = {
+        layout = "diff4_mixed"
+      }
+    }
+  }
+}
