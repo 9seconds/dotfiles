cfd95558229a5cd0893fbc6ccb6dbc884bf55a97
9seconds <nineseconds@yandex.ru>
2024-04-05 08:51:45 +0000

    Better fish aliases

--- fish/.config/fish/conf.d/00-abbr.fish
+++ fish/.config/fish/conf.d/00-abbr.fish
@@ -2,14 +2,15 @@
 
 abbr --add -- gs g s
 abbr --add -- gdf g df
-abbr --add --position anywhere --set-cursor -- L "% | less"
+abbr --add --position anywhere --set-cursor -- L "% &| less"
+abbr --add --position anywhere --set-cursor -- T "&| tee"
 
 function __9s_last_history -d 'Return a latest history item'
   history --max 1
 end
 abbr --add --position anywhere --function __9s_last_history -- !!
 
-function __9s_logfile -d 'Add a tee prefix'
+function __9s_logfile -d 'Generate a name for a logfile'
   set line (commandline -jo | iconv -t ascii//TRANSLIT)
   set prefix ""
 
@@ -22,12 +23,12 @@ function __9s_logfile -d 'Add a tee prefix'
   set line (string shorten -c '' -m 10 -N -- $line)
   set prefix (string join '-' -- $line)-
 
-  echo {$prefix}(date +%s)-(xxd -l 8 -p /dev/random).log
+  printf {$prefix}(date +%s)-(xxd -l 8 -p /dev/random).log
 end
 abbr --add --position anywhere --function __9s_logfile -- LF
 
 function __9s_gitroot -d 'Show git root'
   git rev-parse --show-toplevel 2>/dev/null
-  or echo GR
+  or printf GR
 end
 abbr --add --position anywhere --function __9s_gitroot -- GR
