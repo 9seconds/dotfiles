0802b72fc0387b8c0d6548303f3ce96e654e8136
9seconds <nineseconds@yandex.ru>
2024-04-22 12:39:37 +0000

    Add grapple

---
+++ neovim/.config/nvim/lua/plugins/grapple.lua
@@ -0,0 +1,66 @@
+-- toggling between files I care
+-- https://github.com/cbochs/grapple.nvim
+
+local CLEANUP_EACH = (
+  1000 -- second
+  * 60 -- minute
+  * 60 -- hour
+)
+
+return {
+  "cbochs/grapple.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    {
+      "<leader>a",
+      function()
+        local mod = require("grapple")
+
+        if mod.exists() then
+          vim.notify(" Removed from grapple ", "info")
+          mod.untag()
+        else
+          vim.notify(" Added to grapple ", "info")
+          mod.tag()
+        end
+      end,
+      desc = "Toggle Grapple",
+    },
+    {
+      "<leader>th",
+      function()
+        return require("grapple").toggle_tags()
+      end,
+      desc = "Toggle Grapple menu",
+    },
+    {
+      "gp",
+      function()
+        return require("grapple").cycle_tags("prev", {})
+      end,
+      desc = "Toggle Grapple menu",
+    },
+    {
+      "gp",
+      function()
+        return require("grapple").cycle_tags("next", {})
+      end,
+      desc = "Toggle Grapple menu",
+    },
+  },
+
+  config = function()
+    local mod = require("grapple")
+    local timer = vim.uv.new_timer()
+
+    mod.setup({
+      icons = true,
+      scope = "git_branch",
+      style = "basename",
+      prune = "1d",
+    })
+    timer:start(0, 1000 * 60 * 60, vim.schedule_wrap(mod.prune))
+  end,
+}
