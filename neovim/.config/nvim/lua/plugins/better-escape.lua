2835a282848732cfe1f366eb58ffcd985d6c722a
9seconds <nineseconds@yandex.ru>
2024-07-05 07:08:02 +0000

    Upgrade better-escape

--- neovim/.config/nvim/lua/plugins/better-escape.lua
+++ neovim/.config/nvim/lua/plugins/better-escape.lua
@@ -3,10 +3,9 @@
 
 return {
   "max397574/better-escape.nvim",
+  version = "*",
   event = "InsertEnter",
   main = "better_escape",
 
-  opts = {
-    mapping = { "jj", "kk" },
-  },
+  opts = {},
 }
