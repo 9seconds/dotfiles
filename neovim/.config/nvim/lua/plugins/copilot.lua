1ff30ce7c242498d598f614221112ced90ce66bb
9seconds <nineseconds@yandex.ru>
2024-07-30 19:09:27 +0000

    Add copilot chat

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,24 @@
 -- Copilot stuff
 -- https://github.com/zbirenbaum/copilot.lua
 
-return {
+local copilot_config = {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   keys = {
+    {
+      "<leader>cc",
+      function()
+        require("_.copilot"):user_toggle()
+      end,
+      mode = { "n", "o" },
+      desc = "Toggle Copilot",
+    },
     {
       "<C-c>",
       function()
         require("_.copilot"):user_toggle()
       end,
-      mode = { "n", "i", "o" },
+      mode = "i",
       desc = "Toggle Copilot",
     },
   },
@@ -34,4 +42,66 @@ return {
     })
     require("_.copilot"):activate()
   end,
+}
+
+local copilot_chat_config = {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  version = "*",
+  dependencies = {
+    "zbirenbaum/copilot.lua",
+    "nvim-lua/plenary.nvim",
+  },
+  cmd = {
+    "CopilotChat",
+    "CopilotChatOpen",
+    "CopilotChatClose",
+    "CopilotChatToggle",
+    "CopilotChatStop",
+    "CopilotChatReset",
+    "CopilotChatSave",
+    "CopilotChatLoad",
+    "CopilotChatDebugInfo",
+    "CopilotChatModels",
+    "CopilotChatExplain",
+    "CopilotChatReview",
+    "CopilotChatFix",
+    "CopilotChatOptimize",
+    "CopilotChatDocs",
+    "CopilotChatTests",
+    "CopilotChatFixDiagnostic",
+    "CopilotChatCommit",
+    "CopilotChatCommitStaged",
+  },
+  keys = {
+    {
+      "<leader>ca",
+      function()
+        vim.ui.input({ prompt = "Ask Copilot: " }, function(text)
+          text = text or ""
+          if text ~= "" then
+            require("CopilotChat").ask(
+              text,
+              { selection = require("CopilotChat.select").buffer }
+            )
+          end
+        end)
+      end,
+      mode = { "n", "v" },
+      desc = "Open Copilot chat",
+    },
+    {
+      "<leader>cr",
+      function()
+        require("CopilotChat").reset()
+      end,
+      desc = "Reset Copilot chat",
+    },
+  },
+
+  opts = {},
+}
+
+return {
+  copilot_config,
+  copilot_chat_config,
 }
