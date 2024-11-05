e508a17dfa1942f490ada1c6155dffd0ddf5026a
9seconds <nineseconds@yandex.ru>
2024-11-05 20:20:21 +0000

    Small refactorings for l and ll

--- fish/.config/fish/functions/l.fish
+++ fish/.config/fish/functions/l.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
+set cmd ls --hyperlink=auto
+set wraps ls
+
 if command -q eza
   set cmd eza --classify --icons --git --hyperlink
   set wraps eza
-else
-  set cmd ls --hyperlink=auto
-  set wraps ls
 end
 
 function l -d 'list files' -w $wraps -V cmd
--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
+set cmd ls -lh
+set wraps ls
+
 if command -q eza
   set cmd eza --classify --icons --git --long --binary --header
   set wraps eza
-else
-  set cmd ls -lh
-  set wraps ls
 end
 
 function ll -d 'list files' -w $wraps -V cmd
