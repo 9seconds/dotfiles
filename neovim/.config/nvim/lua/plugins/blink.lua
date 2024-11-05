642da050bdd9c7cca86197358bcf7838d36da9d3
9seconds <nineseconds@yandex.ru>
2024-11-05 12:52:09 +0000

    Hide AI if autocomplete was forcefully triggered

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,11 @@
 -- autocompletion
 -- https://github.com/Saghen/blink.cmp
 
+local function enable_blink(cmp)
+  vim.b.copilot_suggestion_auto_trigger = false
+  cmp.show()
+end
+
 return {
   "Saghen/blink.cmp",
   version = "*",
@@ -16,17 +21,17 @@ return {
         "fallback",
       },
       ["<C-n>"] = {
-        "show",
+        enable_blink,
         "select_next",
         "fallback",
       },
       ["<C-p>"] = {
-        "show",
+        enable_blink,
         "select_prev",
         "fallback",
       },
       ["<C-d>"] = {
-        "show",
+        enable_blink,
         "show_documentation",
         "hide_documentation",
         "fallback",
