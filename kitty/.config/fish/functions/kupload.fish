13771b21c215260abd2880c7ff415016f11eb906
9seconds <nineseconds@yandex.ru>
2024-11-06 11:28:45 +0100

    Add kitty helpers for file transfers

---
+++ kitty/.config/fish/functions/kupload.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+function kdownload -d 'download files using kitty'
+  if test (count $argv) -lt 2
+    echo "Usage: kupload <main computer path> <paths on this machine>"
+    return 1
+  end
+
+  set config_dir ~/.config
+  if set -q XDG_CONFIG_DIR
+    set config_dir $XDG_CONFIG_DIR
+  end
+
+  set home (string escape --regex (realpath ~))
+  set argv[1] (string replace -r "^$home" '~' $argv[1])
+
+  kitten transfer --direction=upload --confirm-paths $argv
+end
---
+++ kitty/.config/fish/functions/ssh.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+function ssh -w ssh -d 'kitten ssh wrapper'
+  if not set -q SSH_CONNECTION
+    kitten ssh $argv
+  else
+    ssh $argv
+  end
+end
