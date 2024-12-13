b6282995b13701d22f05c114951f9c7bd8a6a595
9seconds <nineseconds@yandex.ru>
2024-12-13 15:44:31 +0000

    Fix recursion when connecting to other hosts

--- kitty/.config/fish/functions/ssh.fish
+++ kitty/.config/fish/functions/ssh.fish
@@ -2,11 +2,11 @@
 
 function ssh -w ssh -d "ssh wrapper that is aware of kitten"
   set local_socket (string replace -r "^.*://" "" $KITTY_LISTEN_ON)
-  set cmd ssh -R $local_socket:$local_socket -o SendEnv=KITTY_LISTEN_ON
+  set opts -R $local_socket:$local_socket -o SendEnv=KITTY_LISTEN_ON
 
   if not set -q SSH_CONNECTION
-    set cmd kitten $cmd
+    kitten ssh $opts $argv
+  else
+    command ssh $opts $argv
   end
-
-  $cmd $argv
 end
