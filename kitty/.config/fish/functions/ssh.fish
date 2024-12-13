ec5e57f9d2d7290c90c58df8aa5aedbfca483344
9seconds <nineseconds@yandex.ru>
2024-12-13 16:01:23 +0100

    Correct propagation of kitty stuff

--- kitty/.config/fish/functions/ssh.fish
+++ kitty/.config/fish/functions/ssh.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/env fish
 
-function ssh -w ssh -d 'kitten ssh wrapper'
+function ssh -w ssh -d "ssh wrapper that is aware of kitten"
+  set local_socket (string replace -r "^.*://" "" $KITTY_LISTEN_ON)
+  set cmd ssh -R $local_socket:$local_socket -o SendEnv=KITTY_LISTEN_ON
+
   if not set -q SSH_CONNECTION
-    kitten ssh $argv
-  else
-    command ssh $argv
+    set cmd kitten $cmd
   end
+
+  $cmd $argv
 end
