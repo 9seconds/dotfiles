a0b1738564b78344d8538a3a343283e9b176474a
9seconds <nineseconds@yandex.ru>
2025-01-03 11:22:57 +0000

    Add more snippets

--- neovim/.config/nvim/snippets/python.lua
+++ neovim/.config/nvim/snippets/python.lua
@@ -16,7 +16,7 @@ return {
 
   ["Type definition"] = {
     prefix = "t",
-    body = "$1: ${2:type}"
+    body = "$1: ${2:type}",
   },
 
   ["'while' loop"] = {
@@ -73,5 +73,67 @@ return {
       "def ${1:func}(self, ${2:args}) -> ${3:object}:",
       "\t$0",
     },
+  },
+
+  ["Breakpoint"] = {
+    prefix = "b",
+    body = "breakpoint()$0",
+  },
+
+  ["Conditional breakpoint"] = {
+    prefix = "bc",
+    body = {
+      "if $1:",
+      "\tbreakpoint()$0",
+    },
+  },
+
+  ["Debug statement"] = {
+    prefix = "bd",
+    body = {
+      "try:",
+      "\t${0:$NS_SELECTED_TEXT}",
+      "except:",
+      "\timport sys, traceback",
+      "\ttyp_, val_, tb_ = sys.exc_info()",
+      "\ttback_ = traceback.format_tb(tb_)",
+      "\tbreakpoint()",
+      "\tassert True",
+    },
+  },
+
+  ["'try/except' clause"] = {
+    prefix = "te",
+    body = {
+      "try:",
+      "\t${0:$NS_SELECTED_TEXT}",
+      "except${1: ${2:Exception}${3: as ${4:exc}}}:",
+      "\t${5:pass}",
+    },
+  },
+
+  ["'try/finally' clause"] = {
+    prefix = "tf",
+    body = {
+      "try:",
+      "\t${0:$NS_SELECTED_TEXT}",
+      "finally:",
+      "\t${1:pass}",
+    },
+  },
+
+  ["Magic method"] = {
+    prefix = "__",
+    body = "__$1__",
+  },
+
+  ["Multiline string with double quotes"] = {
+    prefix = "#",
+    body = '"""$0"""',
+  },
+
+  ["Multiline string with single quotes"] = {
+    prefix = "#",
+    body = "'''$0'''",
   },
 }
