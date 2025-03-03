8fb43a24275490c1ef4b8e3cb6ddedf4525c2bb9
9seconds <nineseconds@yandex.ru>
2025-03-03 15:30:00 +0000

    Add colors to fzf

--- fish/.config/fish/conf.d/00-fzf.fish
+++ fish/.config/fish/conf.d/00-fzf.fish
@@ -17,3 +17,21 @@ if command -q fd
 end
 
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+# https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_storm.sh
+set -gx FZF_DEFAULT_OPTS  --color=bg+:#2e3c64 \
+  --color=bg:#1f2335 \
+  --color=border:#29a4bd \
+  --color=fg:#c0caf5 \
+  --color=gutter:#1f2335 \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#29a4bd \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c
