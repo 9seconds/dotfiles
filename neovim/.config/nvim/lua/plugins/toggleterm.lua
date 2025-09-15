f7fb337562397ca306f28596017f5568cdfc8772
9seconds <nineseconds@yandex.ru>
2025-09-15 17:17:27 +0000

    Use snacks picker for selecting terminal

--- neovim/.config/nvim/lua/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,68 +1,129 @@
 -- terminal management
 -- https://github.com/akinsho/toggleterm.nvim
 
+local LINES_IN_PREVIEW = 100
+
 local STATE = {
   visible = false,
   counter = 1,
 }
 
 function STATE:new(direction)
+  self:run(self.counter, direction)
+  self.counter = self.counter + 1
+end
+
+function STATE:run(number, direction)
   if self.visible then
     vim.cmd("ToggleTermToggleAll")
   end
 
   vim.cmd(
-    ("%dToggleTerm direction=%s name=%d"):format(
-      self.counter,
-      direction,
-      self.counter
-    )
+    ("%dToggleTerm direction=%s name=%d"):format(number, direction, number)
   )
-  self.counter = self.counter + 1
 end
 
-function STATE:choose(direction)
+function STATE:choose()
   local terminals = require("toggleterm.terminal").get_all()
+  local snacks = require("snacks")
 
   if #terminals == 0 then
-    return self:new(direction)
+    return self:new("vertical")
   end
 
-  local function switch(terminal)
-    if not terminal then
-      return
-    end
+  snacks.picker({
+    title = "Choose terminal",
+    items = terminals,
 
-    if self.visible then
-      vim.cmd("ToggleTermToggleAll")
-    end
+    preview = function(ctx)
+      local bufnr = tonumber(ctx.item.bufnr)
 
-    vim.cmd(
-      ("%dToggleTerm direction=%s name=%d"):format(
-        terminal.id,
-        direction,
-        terminal.id
-      )
-    )
-  end
+      if not bufnr then
+        return
+      end
 
-  if #terminals == 1 then
-    return switch(terminals[1])
-  end
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+      local total = #lines
+      local start = math.max(1, total - LINES_IN_PREVIEW + 1)
+      local tail = {}
 
-  vim.ui.select(terminals, {
-    prompt = "Please select a terminal",
-    format_item = function(term)
-      return ("%d (%s -> %s)"):format(term.id, term.cmd, term.dir)
+      for i = start, total do
+        table.insert(tail, lines[i])
+      end
+
+      ctx.preview:set_lines(tail)
+      ctx.preview:highlight({
+        ft = vim.bo[bufnr].filetype,
+      })
+
+      return true
     end,
-  }, function(_, idx)
-    switch(terminals[idx])
-  end)
+
+    format = function(item)
+      return {
+        { tostring(item.id), "SnacksPickerLabel" },
+      }
+    end,
+
+    actions = {
+      open_horizontal = function(picker, item)
+        picker:close()
+        self:run(item.id, "horizontal")
+      end,
+      open_vertical = function(picker, item)
+        picker:close()
+        self:run(item.id, "vertical")
+      end,
+      open_tab = function(picker, item)
+        picker:close()
+        self:run(item.id, "tab")
+      end,
+      open_float = function(picker, item)
+        picker:close()
+        self:run(item.id, "float")
+      end,
+    },
+
+    win = {
+      input = {
+        keys = {
+          ["<c-s>"] = {
+            "open_horizontal",
+            mode = { "n", "i" },
+            desc = "Open in horizontal split",
+          },
+          ["<c-v>"] = {
+            "open_vertical",
+            mode = { "n", "i" },
+            desc = "Open in vertical split",
+          },
+          ["<c-t>"] = {
+            "open_tab",
+            mode = { "n", "i" },
+            desc = "Open in tab",
+          },
+          ["<c-f>"] = {
+            "open_float",
+            mode = { "n", "i" },
+            desc = "Open in float window",
+          },
+        },
+      },
+    },
+
+    confirm = function(picker, item)
+      picker:close()
+      self:run(item.id, "vertical")
+    end,
+  })
 end
 
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
   cmd = {
     "TermSelect",
     "TermExec",
@@ -76,60 +137,38 @@ return {
   },
   keys = {
     {
-      "<leader>]V",
+      "<leader>]v",
       function()
         STATE:new("vertical")
       end,
       desc = "Open new vertical terminal",
     },
     {
-      "<leader>]S",
+      "<leader>]s",
       function()
         STATE:new("horizontal")
       end,
       desc = "Open new horizontal terminal",
     },
     {
-      "<leader>]T",
+      "<leader>]t",
       function()
         STATE:new("tab")
       end,
       desc = "Open new tab terminal",
     },
     {
-      "<leader>]F",
+      "<leader>]f",
       function()
         STATE:new("edit")
       end,
       desc = "Open new float terminal",
     },
     {
-      "<leader>]v",
+      "<leader>]]",
       function()
-        STATE:choose("vertical")
+        STATE:choose()
       end,
-      desc = "Switch to vertical terminal",
-    },
-    {
-      "<leader>]s",
-      function()
-        STATE:choose("horizontal")
-      end,
-      desc = "Switch to horizontal terminal",
-    },
-    {
-      "<leader>]t",
-      function()
-        STATE:choose("tab")
-      end,
-      desc = "Switch to tab terminal",
-    },
-    {
-      "<leader>]f",
-      function()
-        STATE:choose("float")
-      end,
-      desc = "Switch to float terminal",
     },
   },
 
@@ -156,6 +195,9 @@ return {
       shell = function()
         return vim.env.SHELL or vim.o.shell or "/bin/bash"
       end,
+
+      persist_mode = false,
+      insert_mappings = false,
     })
 
     local group = vim.api.nvim_create_augroup("9_ToggleTerm", {})
@@ -171,9 +213,16 @@ return {
         vim.keymap.set("t", "<C-j>", "<c-\\><c-n><c-w>j", opts)
         vim.keymap.set("t", "<C-k>", "<c-\\><c-n><c-w>k", opts)
         vim.keymap.set("t", "<C-l>", "<c-\\><c-n><c-w>l", opts)
-        vim.keymap.set("t", "<A-]>", "<c-\\><c-n>", opts)
-        vim.keymap.set("t", "<A-[>", "<cmd>ToggleTerm<cr>", opts)
+        vim.keymap.set("t", "<A-q>", "<c-\\><c-n>", opts)
+        vim.keymap.set("t", "<A-z>", "<cmd>ToggleTerm<cr>", opts)
       end,
+    })
+    vim.api.nvim_create_autocmd("WinEnter", {
+      group = group,
+      pattern = "term://*toggleterm*",
+      callback = vim.schedule_wrap(function()
+        vim.cmd("startinsert")
+      end),
     })
   end,
 }
