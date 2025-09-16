2f335d0142980a6b8677f92d3dc6ad4ca435eddf
9seconds <nineseconds@yandex.ru>
2025-09-16 13:47:44 +0000

    Do not fail if kitten is absent

--- kitty/.config/fish/functions/ssh.fish
+++ kitty/.config/fish/functions/ssh.fish
@@ -4,7 +4,7 @@ function ssh -w ssh -d "ssh wrapper that is aware of kitten"
   set local_socket (string replace -r "^.*://" "" $KITTY_LISTEN_ON)
   set opts -R $local_socket:$local_socket -o SendEnv=KITTY_LISTEN_ON
 
-  if not set -q SSH_CONNECTION
+  if not set -q SSH_CONNECTION; and command -v kitten >/dev/null
     kitten ssh $opts $argv
   else
     command ssh $opts $argv
