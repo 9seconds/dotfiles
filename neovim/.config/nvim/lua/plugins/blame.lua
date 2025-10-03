e751df34d43af4b94c8dc709e1192251eb796392
9seconds <nineseconds@yandex.ru>
2025-10-03 10:15:29 +0000

    Add blame

---
+++ neovim/.config/nvim/lua/plugins/blame.lua
@@ -0,0 +1,18 @@
+-- git blame
+-- https://github.com/FabijanZulj/blame.nvim
+
+return {
+  "FabijanZulj/blame.nvim",
+  cmd = "BlameToggle",
+  keys = {
+    {
+      "<leader>gb",
+      "<cmd>BlameToggle virtual<cr>",
+      desc = "Git blame as virtual text"
+    },
+  },
+
+  opts = {
+    blame_options = { "-w" },
+  },
+}
