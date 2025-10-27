537412a975269f13d829fbd27e2eeb9d8c973932
9seconds <nineseconds@yandex.ru>
2025-10-27 11:57:15 +0000

    Add _mksc script

---
+++ bin/.local/share/mksc_template.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+# vim: set sw=2 sts=2 ts=2
+
+# Show the contents of a script with a given name
+
+set this_script_path $(path resolve $(status --current-filename))
+set this_script_name $(path basename $this_script_path)
+
+argparse 'h/help' 'd/debug' -- $argv
+or return 1
+
+if set -q _flag_debug
+  set fish_trace 1
+end
+
+if set -q _flag_help
+  or test (count $argv) -ne 1
+  echo "Usage: $(basename $this_script_path) [-h/--help] ARG"
+  echo
+  echo "New great script"
+  echo
+  echo "Options:"
+  echo "  -h, --help    Show this help message and exit"
+  return 0
+end
+
+exec echo 1
