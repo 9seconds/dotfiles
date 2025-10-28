41ed2c105963fb7d40dd88cca81fe26439f5aeb0
9seconds <nineseconds@yandex.ru>
2025-10-28 11:01:08 +0100

    Small edits

--- bin/.local/share/mksc_template.fish
+++ bin/.local/share/mksc_template.fish
@@ -1,26 +1,32 @@
 #!/usr/bin/env fish
 # vim: set sw=2 sts=2 ts=2
 
-# Show the contents of a script with a given name
+# Description
 
 set this_script_path $(path resolve $(status --current-filename))
 set this_script_name $(path basename $this_script_path)
 
-argparse 'h/help&' 'd/debug&' -- $argv
-or return 1
+function show_help
+  echo "Usage: $this_script_name [-h/--help] [-d/--debug] ARG"
+  echo
+  echo 'New great script'
+  echo
+  echo 'Options:'
+  echo '  -h, --help    Show this help message and exit'
+  echo '  -d, --debug   Run in debug mode'
+end
+
+argparse --min-args=1 --max-args=1 'h/help&' 'd/debug&' -- $argv
+or begin
+  show_help
+  return 1
+end
 
 if set -q _flag_debug
   set fish_trace 1
 end
-
 if set -q _flag_help
-  or test (count $argv) -ne 1
-  echo "Usage: $(basename $this_script_path) [-h/--help] [-d/--debug] ARG"
-  echo
-  echo "New great script"
-  echo
-  echo "Options:"
-  echo "  -h, --help    Show this help message and exit"
+  show_help
   return 0
 end
 
