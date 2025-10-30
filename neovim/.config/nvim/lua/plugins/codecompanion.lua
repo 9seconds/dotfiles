607892684dc013d14c3f3b6508c6cb3682f04c79
9seconds <nineseconds@yandex.ru>
2025-10-30 17:10:41 +0000

    Better codecompanion default

--- neovim/.config/nvim/lua/plugins/codecompanion.lua
+++ neovim/.config/nvim/lua/plugins/codecompanion.lua
@@ -59,6 +59,14 @@ return {
               },
             },
           },
+
+          variables = {
+            buffer = {
+              opts = {
+                default_params = "watch",
+              },
+            },
+          },
         },
       },
 
