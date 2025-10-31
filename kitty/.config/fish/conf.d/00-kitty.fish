351b3711070a532fc08e8ef5338c1df07f830042
9seconds <nineseconds@yandex.ru>
2025-10-31 11:12:50 +0000

    Add kitty bin to PATH

--- kitty/.config/fish/conf.d/00-kitty.fish
+++ kitty/.config/fish/conf.d/00-kitty.fish
@@ -17,4 +17,5 @@ if set -q KITTY_INSTALLATION_DIR
   set --global KITTY_SHELL_INTEGRATION enabled
   source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
   set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+  fish_add_path --path "$KITTY_INSTALLATION_DIR/kitty/bin"
 end
