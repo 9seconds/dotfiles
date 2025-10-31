2f8bf46499d494478e9fb453570c7675f057db3d
9seconds <nineseconds@yandex.ru>
2025-10-31 12:05:00 +0100

    Add manual integration of kitty

---
+++ kitty/.config/fish/conf.d/00-kitty.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+if not set -q KITTY_INSTALLATION_DIR
+  set    order /Applications/kitty.app/Contents/Resources/kitty
+  set -a order /usr/lib/kitty
+  set -a order ~/.local/share/kitty-ssh-kitten
+
+  for candidate in $order
+    if test -d $candidate
+      set -g KITTY_INSTALLATION_DIR $candidate
+      break
+    end
+  end
+end
+
+if set -q KITTY_INSTALLATION_DIR
+  set --global KITTY_SHELL_INTEGRATION enabled
+  source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+  set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+end
