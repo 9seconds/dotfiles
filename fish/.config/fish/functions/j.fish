027605fbb8c19cf581812611800f61fb667d455b
9seconds <nineseconds@yandex.ru>
2025-11-09 23:31:23 +0000

    Fix j function

--- fish/.config/fish/functions/j.fish
+++ fish/.config/fish/functions/j.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 function j -d 'Fast jump in a recent directory'
-  cd $(zoxide query --list | _fzf --scheme=path --query=$argv[1])
+  cd $(zoxide query --list | 9fzf --scheme=path --query=$argv[1])
 end
