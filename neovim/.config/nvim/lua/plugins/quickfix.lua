10834844c85a141d5e4754ab71920f91546643b5
9seconds <nineseconds@yandex.ru>
2026-01-02 11:09:42 +0000

    Switch quickfix plugins

--- neovim/.config/nvim/lua/plugins/quickfix.lua
+++ neovim/.config/nvim/lua/plugins/quickfix.lua
@@ -1,10 +1,46 @@
 -- pretty quickfix
--- https://github.com/yorickpeterse/nvim-pqf
+-- https://github.com/stevearc/quicker.nvim
 
 return {
-  "yorickpeterse/nvim-pqf",
+  "stevearc/quicker.nvim",
   ft = "qf",
-  main = "pqf",
+  keys = {
+    {
+      "<leader>q",
+      function()
+        require("quicker").toggle()
+      end,
+      desc = "Quicker: Toggle",
+    },
+    {
+      "<leader>a",
+      function()
+        require("quicker").toggle({ loclist = true })
+      end,
+      desc = "Quicker: Toggle loclist",
+    },
+  },
 
-  opts = {},
+  opts = {
+    keys = {
+      {
+        ">",
+        function()
+          require("quicker").expand({
+            before = 2,
+            after = 2,
+            add_to_existing = true,
+          })
+        end,
+        desc = "Expand quickfix context",
+      },
+      {
+        "<",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context",
+      },
+    },
+  },
 }
