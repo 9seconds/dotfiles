02e470c604c8938ef01b0d23315ee1c5cb61738e
9seconds <nineseconds@yandex.ru>
2026-01-19 10:57:30 +0000

    Use another plugin for code diffing

---
+++ neovim/.config/nvim/lua/plugins/codediff.lua
@@ -0,0 +1,12 @@
+-- better code diffing
+-- https://github.com/esmuellert/codediff.nvim
+
+return {
+  "esmuellert/codediff.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "CodeDiff",
+
+  opts = {},
+}
