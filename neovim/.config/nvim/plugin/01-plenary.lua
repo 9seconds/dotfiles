9dd14c19bef09606442aca0bedf1601585d0df49
9seconds <nineseconds@yandex.ru>
2026-04-08 15:52:49 +0200

    Add treesitter

---
+++ neovim/.config/nvim/plugin/01-plenary.lua
@@ -0,0 +1,6 @@
+-- stdlib
+-- https://github.com/nvim-lua/plenary.nvim
+
+require("_.pack").add(
+  "https://github.com/nvim-lua/plenary.nvim"
+)
---
+++ neovim/.config/nvim/plugin/50-treesitter.lua
@@ -0,0 +1,90 @@
+-- treesitter wrappers
+-- https://github.com/neovim-treesitter/nvim-treesitter
+
+local skip_treesitter = {
+  "blink-cmp-documentation",
+  "blink-cmp-menu",
+  "blink-cmp-menu",
+  "blink-cmp-signature",
+  "ergoterm",
+  "flash_prompt",
+  "fzf",
+  "lazy",
+  "lazy_backdrop",
+  "oil",
+  "qf",
+  "snacks_notif",
+  "snacks_terminal",
+  "snacks_win_backdrop",
+}
+
+local ensure_installed = {
+      "bash",
+      "css",
+      "diff",
+      "editorconfig",
+      "fish",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "gomod",
+      "gosum",
+      "html",
+      "ini",
+      "javascript",
+      "json",
+      "just",
+      "regex",
+      "kdl",
+      "lua",
+      "make",
+      "markdown", -- required for code-companion and markview
+      "markdown_inline", -- required for markview
+      "python",
+      "requirements",
+      "ssh_config",
+      "toml",
+      "xml",
+      "yaml",
+}
+
+
+require("_.pack").add(
+  "https://github.com/neovim-treesitter/nvim-treesitter",
+  "main",
+  function()
+    local mod = require("nvim-treesitter")
+
+    mod.install(ensure_installed)
+
+    vim.api.nvim_create_autocmd(
+      "FileType",
+      {
+        group = vim.api.nvim_create_augroup("9_PackTreesitter", {}),
+        callback = function(args)
+          if vim.list_contains(skip_treesitter, args.match) then
+            return
+          end
+
+          local parser_name = vim.treesitter.language.get_lang(args.match)
+          if not parser_name then
+            return
+          end
+
+          local parser_installed =
+            pcall(vim.treesitter.get_parser, args.buf, parser_name)
+          if not parser_installed then
+            mod.install({ parser_name }):wait(30000)
+          end
+
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+          pcall(vim.treesitter.start)
+        end,
+      }
+    )
+  end
+)
