831b71d3e3f39e93718c3480f2dd57f3a1e00bf3
9seconds <nineseconds@yandex.ru>
2026-04-08 15:52:55 +0200

    Add smart paste

---
+++ neovim/.config/nvim/plugin/50-smart-paste.lua
@@ -0,0 +1,9 @@
+-- paste that makes sense
+-- https://github.com/nemanjamalesija/smart-paste.nvim
+
+require("_.pack").add(
+  "https://github.com/nemanjamalesija/smart-paste.nvim",
+  nil,
+  nil,
+  true
+)
