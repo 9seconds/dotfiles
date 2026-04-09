4d29e8e7f2e245e1f7978fdf79b54a92e9221076
9seconds <nineseconds@yandex.ru>
2026-04-09 14:35:41 +0200

    Add copilot

---
+++ neovim/.config/nvim/plugin/50-copilot.lua
@@ -0,0 +1,16 @@
+-- copilot
+-- https://github.com/zbirenbaum/copilot.lua
+
+require("_.pack").add({
+  url = "https://github.com/zbirenbaum/copilot.lua",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = false,
+      }
+    })
+  end
+})
