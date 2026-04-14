494f63b1af15ead64bfb398c2b4145e5a220a156
9seconds <nineseconds@yandex.ru>
2026-04-14 18:11:29 +0000

    Add markview

---
+++ neovim/.config/nvim/plugin/50-markview.lua
@@ -0,0 +1,27 @@
+-- better markdown rendering
+-- https://github.com/OXY2DEV/markview.nvim
+
+vim.pack.add({
+  {
+    src = "https://github.com/OXY2DEV/markview.nvim",
+    version = vim.version.range("*"),
+  },
+})
+
+require("markview").setup({
+  preview = {
+    filetypes = { "markdown", "codecompanion" },
+    ignore_buftypes = {},
+  },
+})
+
+-- codecompanion sets filetype AFTER displaying the buffer, so markview's
+-- BufEnter/BufWinEnter fires too early and skips attachment.
+-- Re-trigger attachment on FileType.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("9_MarkView", {}),
+  pattern = "codecompanion",
+  callback = function(args)
+    require("markview.actions").attach(args.buf)
+  end,
+})
