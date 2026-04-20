3d8c191e1699076d095faa970de547e3ebffcf0a
9seconds <nineseconds@yandex.ru>
2026-04-20 09:13:41 +0200

    Remove cdr and use R environment variable

---
+++ fish/.config/fish/conf.d/00-set_r.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+# vim: set ft=fish sw=2 et sws=2 ts=2
+
+function __9seconds_set_r --on-variable PWD
+  set -f inside (git rev-parse --is-inside-work-tree 2>/dev/null)
+
+  if test (count $inside) -eq 1; and $inside[1] = "true"
+    set -gx R (git rev-parse --show-toplevel 2>/dev/null)
+  else
+    set -eu R
+  end
+end
