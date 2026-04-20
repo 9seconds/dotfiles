c6153ae44a81c2f97c6760a93d491730a2dd6d93
9seconds <nineseconds@yandex.ru>
2026-04-20 12:44:01 +0000

    Optimize setr

--- fish/.config/fish/conf.d/00-set_r.fish
+++ fish/.config/fish/conf.d/00-set_r.fish
@@ -2,13 +2,8 @@
 # vim: set ft=fish sw=2 et sws=2 ts=2
 
 function __9seconds_set_r --on-variable PWD
-  set -f inside (git rev-parse --is-inside-work-tree 2>/dev/null)
-
-  if test (count $inside) -eq 1; and $inside[1] = "true"
-    set -g R (git rev-parse --show-toplevel 2>/dev/null)
-  else
-    set -gu R
-  end
+  set -g R (git rev-parse --show-toplevel 2>/dev/null)
+  or set -gu R
 end
 
 __9seconds_set_r
