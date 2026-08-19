df93c1ff5a07625d2b36d78df90400a9f8c5f9a0
9seconds <nineseconds@yandex.ru>
2026-08-19 07:23:43 +0000

    Add code diff for neovim

---
+++ neovim/.config/nvim/plugin/50-codediff.lua
@@ -0,0 +1,9 @@
+-- vs-code like code diff
+-- https://github.com/esmuellert/codediff.nvim
+
+vim.pack.add({
+  {
+    src = "https://github.com/esmuellert/codediff.nvim",
+    version = vim.version.range("*")
+  }
+})
