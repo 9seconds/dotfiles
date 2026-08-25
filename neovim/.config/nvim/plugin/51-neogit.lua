cf3408975129ca291b2e623ea95c5c213c5b8b2d
9seconds <nineseconds@yandex.ru>
2026-08-25 11:58:49 +0000

    Setup neogit

--- neovim/.config/nvim/plugin/51-neogit.lua
+++ neovim/.config/nvim/plugin/51-neogit.lua
@@ -5,6 +5,15 @@ vim.pack.add({
   "https://github.com/neogitorg/neogit",
 })
 
+require("neogit").setup({
+  integrations = {
+    codediff = true,
+    fzf_lua = true,
+    snacks = false,
+    mini_pick = false,
+  }
+})
+
 vim.keymap.set(
   "n", "<leader>gg",
   function ()
