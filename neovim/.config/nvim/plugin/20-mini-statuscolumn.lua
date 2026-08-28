b0a1369cee69a97fe88c549ddc434159b5fe577a
9seconds <nineseconds@yandex.ru>
2026-08-28 13:20:07 +0000

    Add mini.statuscolumn

---
+++ neovim/.config/nvim/plugin/20-mini-statuscolumn.lua
@@ -0,0 +1,8 @@
+-- improved statuscolumn
+-- https://github.com/nvim-mini/mini.statuscolumn
+
+vim.pack.add({
+  "https://github.com/nvim-mini/mini.statuscolumn",
+})
+
+require("mini.statuscolumn").setup()
