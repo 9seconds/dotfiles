c42506c9f8fea66398f0cd48d68c2b45533fc6ae
9seconds <nineseconds@yandex.ru>
2026-09-10 09:00:57 +0000

    Remove statuscolumn plugin

--- neovim/.config/nvim/plugin/20-mini-statuscolumn.lua
+++
@@ -1,8 +0,0 @@
--- improved statuscolumn
--- https://github.com/nvim-mini/mini.statuscolumn
-
-vim.pack.add({
-  "https://github.com/nvim-mini/mini.statuscolumn",
-})
-
-require("mini.statuscolumn").setup()
